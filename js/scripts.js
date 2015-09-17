c6cfa4daa95dfc348ef4575477a6ad420474f9b4
nataliyamiller <nataliyamiller82@gmail.com>
2015-09-17 16:25:35 -0700

    Passes spec for assigning numeric values to card's names

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
 
+var cardValues = function() {
+  var values = new Array();
+  var cardNames = new Array("Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King");
+  for (var i = 0; i < cardNames.length; i++) {
+    if (cardNames[i] === "Ace") {
+      values.push([cardNames[i], 11]);
+    } else if (cardNames[i] === "Jack" || cardNames[i] === "Queen" || cardNames === "King") {
+      values.push([cardNames[i], 10]);
+    } else {
+      values.push([cardNames[i], parseInt(cardNames[i])])
+    }
+  }
+  return values;
+}
+
+
 var createDeck = function() {
   var deck = [];
   var cardRanks = new Array("Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King");
--- spec/specs.js
+++ spec/specs.js
@@ -1,3 +1,17 @@
+describe('cardValues', function() {
+  it("assigns card names to numeric values", function() {
+    expect(cardValues()[1]).to.eql(["2", 2]);
+  });
+
+  it("assigns card names to numeric values", function() {
+    expect(cardValues()[0]).to.eql(["Ace", 11]);
+  });
+
+  it("assigns card names to numeric values", function() {
+    expect(cardValues()[10]).to.eql(["Jack", 10]);
+  });
+});
+
 describe('createDeck', function() {
   it("returns the deck of 52 cards to play with", function() {
     expect(createDeck().length).to.equal(52);
