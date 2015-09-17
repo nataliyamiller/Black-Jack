bcffba9eb1c4558fccecedfa2d710bf00287da8d
nataliyamiller <nataliyamiller82@gmail.com>
2015-09-17 13:07:16 -0700

    Passes specs for creating a deck of cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
-var deckOfCards = function() {
+// var totalCards = function() {
+//   var cardRanks = new Array("Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King");
+//   var cardSuits = new Array("C", "D", "H", "S");
+//   var totalCards = cardRanks.length * cardSuits.length;
+//   return totalCards;
+// }
+
+var createDeck = function() {
+  var deck = [];
   var cardRanks = new Array("Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King");
   var cardSuits = new Array("C", "D", "H", "S");
-  var deckTotal = cardRanks.length * cardSuits.length;
-  return deckTotal;
+
+  for (var i = 0; i < cardSuits.length; i++) {
+    for (var j = 0; j < cardRanks.length; j++) {
+      deck.push([cardRanks[i], cardSuits[j]]);
+
+    }
+
+  }
+  return deck;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,9 @@
-describe('deckOfCards', function() {
+describe('createDeck', function() {
   it("returns the deck of 52 cards to play with", function() {
-    (expect(deckOfCards())).to.equal(52);
+    expect(createDeck().length).to.equal(52);
+  });
+
+  it("returns a card rank with a correspondng card suit", function() {
+    expect(createDeck()[0]).to.eql(["Ace", "C"]);
   });
 });
