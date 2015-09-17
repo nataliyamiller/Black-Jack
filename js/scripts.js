dc03030686765e7c5cba9ec16b3d071fd23f335e
nataliyamiller <nataliyamiller82@gmail.com>
2015-09-17 16:35:41 -0700

    Some code refactoring done

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,32 @@
 
+var cardNames = function() {
+  var names = new Array("Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King");
+  return names;
+}
+
 var cardValues = function() {
   var values = new Array();
-  var cardNames = new Array("Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King");
-  for (var i = 0; i < cardNames.length; i++) {
-    if (cardNames[i] === "Ace") {
-      values.push([cardNames[i], 11]);
-    } else if (cardNames[i] === "Jack" || cardNames[i] === "Queen" || cardNames === "King") {
-      values.push([cardNames[i], 10]);
+  var names = cardNames();
+  for (var i = 0; i < names.length; i++) {
+    if (names[i] === "Ace") {
+      values.push([names[i], 11]);
+    } else if (names[i] === "Jack" || names[i] === "Queen" || names === "King") {
+      values.push([names[i], 10]);
     } else {
-      values.push([cardNames[i], parseInt(cardNames[i])])
+      values.push([names[i], parseInt(names[i])]);
     }
   }
   return values;
 }
 
-
 var createDeck = function() {
   var deck = [];
-  var cardRanks = new Array("Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King");
-  var cardSuits = new Array("C", "D", "H", "S");
+  var names = cardNames();
+  var suits = new Array("C", "D", "H", "S");
 
-  for (var i = 0; i < cardRanks.length; i++) {
-    for (var j = 0; j < cardSuits.length; j++) {
-      deck.push([cardRanks[i], cardSuits[j]]);
+  for (var i = 0; i < names.length; i++) {
+    for (var j = 0; j < suits.length; j++) {
+      deck.push([names[i], suits[j]]);
     }
   }
   return deck;
